50cd66543d1be3362e2e1084294811ccc18e1f75
Hi-archer <285484026@QQ.COM>
2022-10-31 21:30:13 +0800

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,25 @@
+import time
+import datetime
+import os
+import shutil
+from tqdm import tqdm
+
+def fileTime(file):   
+    t = time.localtime(os.path.getmtime(file))
+    return time.strftime("%Y-%m-%d",t)
+
+files_path = "E:/DCIM/100MSDCF/"
+Tofiles_path = "相机照片/"
+
+files_list = os.listdir(files_path)
+
+for i in tqdm(files_list):
+    date_name = fileTime(files_path + i)
+    # print(files_name)
+    if not os.path.exists(Tofiles_path+date_name):
+        os.mkdir(Tofiles_path+date_name)#调用系统命令行来创建文件
+    file_old_name = files_path + i
+    file_new_name = Tofiles_path + date_name +'/' + i
+    print((file_old_name,file_new_name))
+    if not os.path.exists(file_new_name):
+        shutil.copyfile(file_old_name,file_new_name) 
